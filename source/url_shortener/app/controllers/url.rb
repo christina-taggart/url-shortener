c070d88636c1202bb5f22e45e137559ad6d51733
John Olmsted & Ricardo A. Rubio <pair+qsymmachus.rickarubio@devbootcamp.com>
2014-01-29 15:55:22 -0800

    Add error branch to the /urls route

--- source/url_shortener/app/controllers/url.rb
+++ source/url_shortener/app/controllers/url.rb
@@ -1,9 +1,16 @@
 get '/' do
+  @error = params[:error]
+  @short_key = params{:short_key}
   erb :index
 end
 
 post '/urls' do #create new url
-  Url.new(params)
+  new_url = Url.new(params)
+  if new_url.save
+    redirect '/?short_key=true'
+  else
+    redirect '/?error=true'
+  end
 end
 
 get '/:short_url' do # redirct to long url
