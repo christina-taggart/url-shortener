c40678be077886d2efe27225034cd444b3739c52
Apprentice <apprentice@dbc11.local>
2014-01-29 15:54:28 -0800

    Add route for redirection utility

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -4,7 +4,12 @@ get '/' do
   erb :index
 end
 
-post '/urls/new' do
-  @url = Url.new(full: params[:url], short: Faker::Lorem.characters(char_count=6))
+post '/urls' do
+  @url = Url.create(full: params[:url], short: Faker::Lorem.characters(char_count=6))
   erb :url
+end
+
+get '/:short_url' do
+  url = Url.where(short: params[:short_url]).first
+  redirect ("#{url.full}")
 end
