c2c6945c7c74e91f48d7af02cd007a40d835b677
Christina Taggart & <pair+christina-taggart.hzla00@devbootcamp.com>
2014-01-29 13:32:02 -0800

    created url shortener, never duplicates url, no need for validations

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -1,4 +1,28 @@
+require_relative '../../config/environment'
+require_relative '../../config/database'
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+post '/urls' do
+  @user_url = params[:user_url]
+  @short_url = generate_urls(@user_url)
+  Url.create(short: @short_url, long: @user_url, clicks: 0)
+  erb :short
+end
+
+get '/:short_url' do
+  url = Url.where(short: params[:short_url]).first
+  new_click_count = url.clicks + 1
+  url.update_attributes(clicks: new_click_count)
+  redirect "http://www." + url.long
+end
+
+def generate_urls(user_url)
+  short = rand(1000000)
+  until Url.where(short: short).empty?
+    short = rand(1000000)
+  end
+  short
 end
