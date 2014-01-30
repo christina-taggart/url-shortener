33589d1fe8e0260bbd16bcdf6721376202db1278
Darcey Lachtman & Kevin Yeh <pair+dslachtman.kyeh@devbootcamp.com>
2014-01-29 17:08:00 -0800

    Add links to short_urls and incrementing view counter

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -11,5 +11,9 @@ end
 
 # e.g., /q6bda
 get '/:short_url' do
+  puts "params #{params.inspect}"
   # redirect to appropriate "long" URL
+  @url = Url.get_url_from_short(params[:short_url])
+  @url.increment_count
+  redirect "#{@url.address}"
 end
--- source/url_shortener/app/models/url.rb
+++ source/url_shortener/app/models/url.rb
@@ -1,8 +1,22 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
-  before_save :set_short_url
+  before_create :set_short_url
+  before_create :set_count
 
   def set_short_url
     self.short_url = ('a'..'z').to_a.sample(4).join
+  end
+
+  def set_count
+    self.count = 0
+  end
+
+  def self.get_url_from_short(short)
+    where(short_url: short).first
+  end
+
+  def increment_count
+    self.count += 1
+    save
   end
 end
