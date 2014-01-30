1183f4a2a53bba0175f1384e74748e16bc3871d6
Darcey Lachtman & Kevin Yeh <pair+dslachtman.kyeh@devbootcamp.com>
2014-01-29 16:32:52 -0800

    add functionality to adding short url

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -2,3 +2,13 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/urls' do
+  Url.create(params)
+  redirect '/'
+end
+
+# e.g., /q6bda
+get '/:short_url' do
+  # redirect to appropriate "long" URL
+end
--- source/url_shortener/app/models/url.rb
+++ source/url_shortener/app/models/url.rb
@@ -1,4 +1,8 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
+  before_save :set_short_url
 
+  def set_short_url
+    self.short_url = ('a'..'z').to_a.sample(4).join
+  end
 end
