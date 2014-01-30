06cd2f1d38121e13d2b8adc83c7d4f4bf28f2f80
Apprentice <apprentice@dbc11.local>
2014-01-29 16:16:45 -0800

    add url validations, error message for invalid urls

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -7,7 +7,12 @@ end
 post '/urls' do
   @url = Url.create(full: params[:url], click_count: 0,
                     short: Faker::Lorem.characters(char_count=6))
-  erb :url
+  unless @url.id
+    @error = "Not a valid URL! Please try again"
+    erb :index
+  else
+    erb :url
+  end
 end
 
 get '/:short_url' do
--- source/url_shortener/app/models/url.rb
+++ source/url_shortener/app/models/url.rb
@@ -1,3 +1,5 @@
+require 'uri'
 class Url < ActiveRecord::Base
   validates :short, uniqueness: true
+  validates :full, format: { with: URI::regexp(['http','https']) }
 end
