3be9ca9c077f78b08375dd839024105acb9e473e
Apprentice <apprentice@dbc11.local>
2014-01-30 00:00:07 -0800

    add functional simple shortener for Emi.ly

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -1,4 +1,16 @@
+require 'securerandom'
+
 get '/' do
-  # Look in app/views/index.erb
   erb :index
+end
+
+
+post '/urls' do
+  @url = Url.create(long_url: params[:url], short_url: 'localhost:9393' + '/' + SecureRandom.hex(3))
+  erb :url
+end
+
+get '/:short_url' do
+  short = Url.where(short_url: params[:short_url]).first
+  redirect('#{short}')
 end
