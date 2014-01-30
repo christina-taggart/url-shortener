24b5e3d8210ad5bcfeb73990e83cd624bbb58ad2
Christina Taggart & <pair+christina-taggart.hzla00@devbootcamp.com>
2014-01-29 16:15:35 -0800

    Added validations

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -8,15 +8,20 @@ end
 post '/urls' do
   @user_url = params[:user_url]
   @short_url = generate_urls(@user_url)
-  Url.create(short: @short_url, long: @user_url, clicks: 0)
-  erb :short
+  url = Url.new(short: @short_url, long: @user_url, clicks: 0)
+  if url.valid?
+    url.save
+    erb :short
+  else
+    erb :invalid
+  end
 end
 
 get '/:short_url' do
   url = Url.where(short: params[:short_url]).first
   new_click_count = url.clicks + 1
   url.update_attributes(clicks: new_click_count)
-  redirect "http://www." + url.long
+  redirect modified_url(url.long)
 end
 
 def generate_urls(user_url)
@@ -25,4 +30,12 @@ def generate_urls(user_url)
     short = rand(1000000)
   end
   short
+end
+
+def modified_url(url)
+  if url[0..3] != 'http'
+    'http://' + url
+  else
+    url
+  end
 end
--- source/url_shortener/app/models/url.rb
+++ source/url_shortener/app/models/url.rb
@@ -1,3 +1,13 @@
+require 'uri'
+
 class Url < ActiveRecord::Base
+  validate :validate_url
+
+  def validate_url
+    unless self.long.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
+      errors.add(:long, "not a valid url")
+    end
+  end
+
   # Remember to create a migration!
 end
