8be46444b7a856b720736f9b87d07a92dc43d730
Apprentice <apprentice@dbc11.local>
2014-01-29 16:02:13 -0800

    Add visit counter function to redirect route

--- source/url_shortener/app/controllers/index.rb
+++ source/url_shortener/app/controllers/index.rb
@@ -5,11 +5,14 @@ get '/' do
 end
 
 post '/urls' do
-  @url = Url.create(full: params[:url], short: Faker::Lorem.characters(char_count=6))
+  @url = Url.create(full: params[:url], click_count: 0,
+                    short: Faker::Lorem.characters(char_count=6))
   erb :url
 end
 
 get '/:short_url' do
   url = Url.where(short: params[:short_url]).first
+  url.click_count += 1
+  url.save
   redirect ("#{url.full}")
 end
--- source/url_shortener/db/migrate/20140129152654_urls.rb
+++ source/url_shortener/db/migrate/20140129152654_urls.rb
@@ -1,8 +1,9 @@
 class Urls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
-      t.string :full
-      t.string :short
+      t.string  :full
+      t.string  :short
+      t.integer :click_count
     end
   end
 end
